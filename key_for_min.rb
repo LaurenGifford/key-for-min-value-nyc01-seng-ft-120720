f6fd3ac17c05200949adc93d552d92a43d3d18dd
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-18 00:26:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     min_value = name_hash[name]
     
-      if name_hash[name] < min_value
+      if value < min_value
          min_value = value
       end
       
